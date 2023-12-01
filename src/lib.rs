9e478b6dad73a0b2dc30cff558cac2d869cb191b
Enigmatrix <enigmatrix2000@gmail.com>
2023-12-01 14:49:19 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Seek};
+
+mod day1;
+
+pub fn input(day: u32) -> impl Seek + BufRead {
+    let file = File::open(format!("inputs/{day:02}.txt")).expect("input not found");
+    BufReader::new(file)
+}
